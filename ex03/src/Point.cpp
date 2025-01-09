b85111593854bb8373fc396c51e742a725410cb4
Nigel Robinson <nrobinso@paul-f4Ar8s5.clusters.42paris.fr>
2025-01-09 09:43:16 +0100

    finished

--- ex03/src/Point.cpp
+++ ex03/src/Point.cpp
@@ -6,35 +6,41 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 09:39:47 by nrobinso          #+#    #+#             */
-/*   Updated: 2025/01/09 09:29:05 by nrobinso         ###   ########.fr       */
+/*   Updated: 2025/01/09 09:40:51 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./include/Point.hpp"
 
+// Default constructor
 Point::Point(void) : _x(0), _y(0) {
     
 };
 
+// constructor that takes as parameters two constant floating-point numbers
 Point::Point(const float x, const float y) : _x(x), _y(y) {
     
 };
 
+// Copy constructor
 Point::Point(Point const &point) : _x(point._x), _y(point._y) {
     
 };
 
+// Copy assignment operator
 Point& Point::operator=(const Point& point) {
 
     (void)point;
     return (*this);    
 };
 
+// Destructor
 Point::~Point(void) {
 
     
 };
 
+// get x and y values
 const Fixed Point::get_x(void) const{
 
     return (this->_x);
@@ -45,13 +51,13 @@ const Fixed Point::get_y(void) const{
     return (this->_y);
 };
 
-// output
-std::ostream &operator<<(std::ostream &outputstream, const Point &point) {
+// output for debug
+// std::ostream &operator<<(std::ostream &outputstream, const Point &point) {
     
-    outputstream 
-    << "x : " 
-    << point.get_x() 
-    << " y : " 
-    << point.get_y();
-    return (outputstream);
-};
+//     outputstream 
+//     << "x : " 
+//     << point.get_x() 
+//     << " y : " 
+//     << point.get_y();
+//     return (outputstream);
+// };
